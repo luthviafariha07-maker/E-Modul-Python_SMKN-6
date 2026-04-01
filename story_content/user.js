4b0ccc747fc936a8984ab0c371013b546d77dd83
luthviafariha07-maker <luthviafariha07@gmail.com>
2026-04-01 08:49:02 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,252 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5cJq50qrkJz":
+        Script1();
+        break;
+      case "6P0gObLONdj":
+        Script2();
+        break;
+      case "6f5G2RTyaus":
+        Script3();
+        break;
+      case "5jrkeRP3Y6B":
+        Script4();
+        break;
+      case "5wWB6dY2cdx":
+        Script5();
+        break;
+      case "6YjP79RgOLJ":
+        Script6();
+        break;
+      case "5cADCUk2PTC":
+        Script7();
+        break;
+      case "6W2fDFsZWko":
+        Script8();
+        break;
+      case "5iB6oSR7tSm":
+        Script9();
+        break;
+      case "6QRMdieLLTD":
+        Script10();
+        break;
+      case "5Zf9C4iaCSD":
+        Script11();
+        break;
+      case "6JTktG8J0tr":
+        Script12();
+        break;
+      case "6DyN0kGrhND":
+        Script13();
+        break;
+      case "5fvfthGMq7l":
+        Script14();
+        break;
+      case "5ZwOw7LtEs7":
+        Script15();
+        break;
+      case "6iNoWKoCvMH":
+        Script16();
+        break;
+      case "6jwn2EiiJHM":
+        Script17();
+        break;
+      case "5oUE4alFpBS":
+        Script18();
+        break;
+      case "6JuMT8Lo9xJ":
+        Script19();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script17()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script18()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script19()
+{
+  var audio = document.getElementById('bgSong');
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
